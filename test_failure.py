e2a34189eb742f6b2e664d4a9c1c54f1b08e6893
proffatai <proffatai@gmail.com>
2023-05-20 20:14:02 +0100

    added more test

---
+++ test_failure.py
@@ -0,0 +1,15 @@
+import math
+
+def test_sqrt_failure():
+    num=25
+    assert math.sqrt(num) == 6
+
+def test_square_failure():
+    assert 7*7 == 40
+
+def test_equality_failure():
+    assert 10 == 11
+
+
+# During test execution, use the command: `pytest test_failure.py --maxfail=1`
+#The test execution will stop once 1 test fails and it wont proceed to run the rest of the test
